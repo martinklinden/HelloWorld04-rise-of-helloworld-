6543765a458527181a45ff0660a6934f7e1ce4fc
Martin Lindén <MaritnI@hotmail.com>
2020-10-06 11:26:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld04__rise_of_helloworld_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
